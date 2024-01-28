559769c68d15789e358c564db39f924f50dbaada
Vasilina <vasilinamel04@gmail.com>
2024-01-28 22:25:07 +1000

    Исправлены действия в споснорстве, создано поле контакты

--- frontend/lib/pages/check_data_screen.dart
+++ frontend/lib/pages/check_data_screen.dart
@@ -25,74 +25,69 @@ class HomePage extends StatefulWidget {
 class _HomePage extends State<HomePage> {
   @override
   Widget build(context) => Scaffold(
-    appBar: AppBar(
-      backgroundColor: Color.fromARGB(255, 87, 87, 87),
-      automaticallyImplyLeading: false,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Align(
-          alignment: Alignment.centerLeft,
-          child:
-              SizedBox(
-              width: 80,
-              height: 35,
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                  backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 215, 215, 215)),
-                  padding: MaterialStateProperty.all(EdgeInsets.all(5)),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 87, 87, 87),
+          automaticallyImplyLeading: false,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SizedBox(
+                  width: 80,
+                  height: 35,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 215, 215, 215)),
+                        padding: MaterialStateProperty.all(EdgeInsets.all(5)),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/home');
+                      },
+                      child: const Text('Назад',
+                          style: TextStyle(fontSize: 20, color: Colors.black))),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
-                }, child: const Text('Назад', style: TextStyle(fontSize: 20,color: Colors.black))),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left:25),
-              child: 
-                Text('MARATHON SKILLS 2023', style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30,color: Colors.white),)
-            )
-        ],
-      ),
-    ),
-    body: SafeArea(
-        child:Center(
-          child:
-          Stack(
-            alignment: AlignmentDirectional.bottomEnd,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                child: Content()
-              ),
-              
-              Container(
-                height: 70,
-                width: 110,
-                child: LoginButton()
-              )
-              
-            ]
-          )
-        )
-    ),
-    bottomNavigationBar: Container(
-      padding: EdgeInsets.all(12),
-      height: 50.0,
-      color: Color.fromARGB(255, 87, 87, 87),
-      child: Row(
+              Padding(
+                  padding: EdgeInsets.only(left: 25),
+                  child: Text(
+                    'MARATHON SKILLS 2023',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 30,
+                        color: Colors.white),
+                  ))
+            ],
+          ),
+        ),
+        body: SafeArea(
+            child: Center(
+                child:
+                    Stack(alignment: AlignmentDirectional.bottomEnd, children: [
+          Container(width: MediaQuery.of(context).size.width, child: Content()),
+          Container(height: 70, width: 110, child: LoginButton())
+        ]))),
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.all(12),
+          height: 50.0,
+          color: Color.fromARGB(255, 87, 87, 87),
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('18 дней, 8 часов и 17 минут до старта марафона!', style: TextStyle(fontSize: 18,color: Colors.white),)
+              Text(
+                '18 дней, 8 часов и 17 минут до старта марафона!',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              )
             ],
-    ),
-    ),
-  );
+          ),
+        ),
+      );
 }
 
 class Content extends StatefulWidget {
@@ -105,18 +100,16 @@ class Content extends StatefulWidget {
 class _Content extends State<Content> {
   @override
   Widget build(context) =>
-    Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return AnimatedContainer(
+      Scaffold(body: LayoutBuilder(builder: (context, constraints) {
+        return AnimatedContainer(
             duration: Duration(milliseconds: 500),
             color: Color.fromARGB(255, 255, 255, 255),
-            padding: constraints.maxHeight < 500 ? EdgeInsets.zero : EdgeInsets.all(30.0) ,
+            padding: constraints.maxHeight < 500
+                ? EdgeInsets.zero
+                : EdgeInsets.all(30.0),
             child: Center(
               child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 30.0, horizontal: 25.0
-                ),
+                padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 25.0),
                 constraints: BoxConstraints(
                   maxWidth: 600,
                   maxHeight: 280,
@@ -126,48 +119,54 @@ class _Content extends State<Content> {
                   borderRadius: BorderRadius.circular(5.0),
                 ),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(
-                      width: 350,
-                      height: 80,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 233, 233, 233)),
-                          padding: MaterialStateProperty.all(EdgeInsets.all(5))
-                        ),
-                          onPressed: () {Navigator.pushNamed(context, '/check');}, 
-                          child: const Text('Я участвовал ранее', style: TextStyle(fontSize: 20,color: Colors.black))),
-                    ),
-                    SizedBox(
-                      width: 350,
-                      height: 80,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 233, 233, 233)),
-                          padding: MaterialStateProperty.all(EdgeInsets.all(5))
-                        ),
-                          onPressed: () {Navigator.pushNamed(context, '/runner_sponsor');}, 
-                          child: const Text('Я новый участник', style: TextStyle(fontSize: 20,color: Colors.black))),
-                    ),
-                  ]
-                ),
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 350,
+                        height: 80,
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                                backgroundColor: MaterialStateProperty.all(
+                                    Color.fromARGB(255, 233, 233, 233)),
+                                padding: MaterialStateProperty.all(
+                                    EdgeInsets.all(5))),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/login');
+                            },
+                            child: const Text('Я участвовал ранее',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black))),
+                      ),
+                      SizedBox(
+                        width: 350,
+                        height: 80,
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                                shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                                backgroundColor: MaterialStateProperty.all(
+                                    Color.fromARGB(255, 233, 233, 233)),
+                                padding: MaterialStateProperty.all(
+                                    EdgeInsets.all(5))),
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/runner_sponsor');
+                            },
+                            child: const Text('Я новый участник',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black))),
+                      ),
+                    ]),
               ),
-            )
-          );
-        }
-      )
-   );
+            ));
+      }));
 }
 
 class LoginButton extends StatefulWidget {
@@ -180,10 +179,8 @@ class LoginButton extends StatefulWidget {
 class _LoginButton extends State<LoginButton> {
   @override
   Widget build(context) =>
-    Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          return AnimatedContainer(
+      Scaffold(body: LayoutBuilder(builder: (context, constraints) {
+        return AnimatedContainer(
             duration: Duration(milliseconds: 500),
             color: Color.fromARGB(255, 255, 255, 255),
             child: Align(
@@ -194,24 +191,22 @@ class _LoginButton extends State<LoginButton> {
                   maxWidth: 180,
                   maxHeight: 100,
                 ),
-                child: 
-                    ElevatedButton(
-                      style: ButtonStyle(
+                child: ElevatedButton(
+                    style: ButtonStyle(
                         shape: MaterialStateProperty.all(
                           RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(10),
                           ),
                         ),
-                        backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 215, 215, 215)),
+                        backgroundColor: MaterialStateProperty.all(
+                            Color.fromARGB(255, 215, 215, 215)),
                         padding: MaterialStateProperty.all(EdgeInsets.all(15)),
-                        textStyle: MaterialStateProperty.all(TextStyle(fontSize: 20))
-                      ),
-                        onPressed: () {}, child: const Text('Login', style: TextStyle(fontSize: 20,color: Colors.black))
-                    ),
-                ),
-            )
-          );
-        }
-      )
-   );
+                        textStyle:
+                            MaterialStateProperty.all(TextStyle(fontSize: 20))),
+                    onPressed: () {},
+                    child: const Text('Login',
+                        style: TextStyle(fontSize: 20, color: Colors.black))),
+              ),
+            ));
+      }));
 }
